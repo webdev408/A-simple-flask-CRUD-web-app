062070634c83764ae8f902014116aee73690cb2e
Your Name <you@example.com>
2025-01-23 01:49:06 -0500

    Added a user

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ class User(db.Model):
   image = db.Column(db.String(120), unique=True, nullable=False)
 
   def __repr__(self):
-    return '<User %r>' % self.username
+    return '<User %r>' % self.name
   
 @app.route('/')
 def index():
